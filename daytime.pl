df0efb05dc28e854405f8e7a4f984dd09283b1c3
Nathan Dautenhahn <ndd@rice.edu>
2020-03-23 13:30:39 -0500

    allow task to have pomos for multiple days
    Signed-off-by: Nathan Dautenhahn <ndd@rice.edu>

--- daytime.pl
+++ daytime.pl
@@ -5,10 +5,15 @@ use strict;
 my %data = ('s' => 0, 'm' => 0, 't' => 0, 'w' => 0, 'r' => 0, 'f' => 0, 'a' => 0);
 
 while(<>){
-    if(/\[.\].*\[(.*)\|.*\|(\d+(\.\d*)?|)\|(s:(x+)|m:(x+)|t:(x+)|w:(x+)|r:(x+)|f:(x+)|a:(x+))\]/){
-        my ($day, $pomos) = split(':', $4);
-        #print $_, "- Day:", $day, ", pomos: ", $pomos, "\n";
-        $data{$day} += length($pomos);
+    #if(/\[.\].*\[(.*)\|.*\|(\d+(\.\d*)?|)\|(s:x+|m:x+|t:x+|w:x+|r:x+|f:x+|a:x+)\]/){
+    #if(/\[.\].*\[(.*)\|.*\|(\d+(\.\d*)?|)\|(s:x+)*,*(m:x+)*,*(t:x+)*,*(w:x+)*,*(r:x+)*,*(f:x+)*,*(a:x+)*\]/){
+    if(/\[.\].*\[(.*)\|.*\|(\d+(\.\d*)?|)\|([smtwrfa]+.*)\]/){
+        my @pomos = split(',',$4);
+        foreach (@pomos)
+        {
+            my ($day, $pomos) = split(':', $_);
+            $data{$day} += length($pomos);
+        }
     }
 }
 print "s:",$data{'s'};
