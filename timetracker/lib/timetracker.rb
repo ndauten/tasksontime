68c1a1017c522a5543b367c720dce1438cbcbddc
Claire <ch107@rice.edu>
2023-09-08 17:15:10 -0500

    fix bugs and add README

--- timetracker/lib/timetracker.rb
+++ timetracker/lib/timetracker.rb
@@ -1,7 +1,6 @@
 require 'timetracker/version.rb'
 
-require 'timetracker/brick_tree.rb'
+require '/home/claire/installs/tasksontime/timetracker/lib/timetracker/brick_tree.rb'
 require 'timetracker/timer.rb'
-
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
